57ab4ab8adbf1c338ec4898947612df4cd5106e6
Nicolas <nicdelgado03@Gmail.com>
2026-03-27 21:19:54 -0500

    Completed Ladder functionality / Imported Key / Started Item Prompt System

---
+++ Scenes/key.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+#func _on_button_clicked():
+	#if pointer is "player":
+		#queue_free()
+
+
+# Layout:
+# When plyers mouse hover over the key, no greater than 5 meters or so, a pickup defined by 'E' will appear
+# Upon clicking 'E', item will disappear from the world and the RE style inspect happens
+# Pressing 'E' will either collect or put down the item, depending on its importance
---
+++ Scenes/ladder.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+	self.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.name == "player":
+		body.ladder_array.append(self)
+		body.current_state = body.State.LADDER
+	
+func _on_body_exited(body):
+	if body.name == "player":
+		body.ladder_array.erase(self)
+		if body.ladder_array.size() == 0:
+			body.current_state = body.State.NORMAL
