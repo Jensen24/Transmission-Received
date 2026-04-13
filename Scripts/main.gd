8101fffcc0d8937ed9f7c3f4c74b3799a2957384
Nicolas <nicdelgado03@Gmail.com>
2026-04-12 21:04:08 -0500

    Push up to date

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,9 +2,7 @@ extends Node
 
 @onready var player = $World/Player
 @onready var pause_menu: Control = $HUD/PauseMenu
-#var inspect_scene = preload("res://Scenes/inspect_system.tscn")
-#var inspecting := false
-#var inspect_instance = null
+
 var paused = false
 
 func _ready() -> void:
@@ -16,29 +14,6 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
 		
-#func start_inspect(item_scene: PackedScene):
-	#if inspecting:
-		#return
-	#inspecting = true
-	#player.set_process(false)
-	#
-	#inspect_instance = inspect_scene.instantiate()
-	#add_child(inspect_instance)
-	#inspect_instance.set_item(item_scene)
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#
-#func stop_inspect():
-	#inspecting = false
-	#
-	#player.set_process(true)
-	#inspect_instance.queue_free()
-	#inspect_instance = null
-	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-#func _input(event):
-	#if Input.is_action_just_pressed("interact"):
-		#if inspecting:
-			#stop_inspect()
 
 func _on_resume_game():
 	pause_menu.hide()
